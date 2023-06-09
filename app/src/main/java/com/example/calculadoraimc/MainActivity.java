0c383dcc48a0c600dc0cfce7ab3a8f88e72bad78
mikel29 <veces21@gmail.com>
2023-06-09 08:24:22 -0500

    en esta agregamos los snnipers calcular los pesos en kg libras y cm y metro tambien agregue si ESTA EN SOBRE PESO Y ESO

--- app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.calculadoraimc;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText txtEdad,txtAltura,txtPeso;
-    private Button  btnMasculino,btnFemenino,btnCalcularIMC;
-
-    private TextView textViewResultado;
+    private EditText txtEdad, txtAltura, txtPeso;
+    private ImageButton btnMasculino, btnFemenino;
+    private Button btnCalcularIMC;
+    private TextView textViewResultado, textViewSituacion;
+    private Spinner altura, peso;
 
     private String selectedGender;
+    private int edad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +32,12 @@ public class MainActivity extends AppCompatActivity {
         btnFemenino = findViewById(R.id.btnFemenino);
         txtPeso = findViewById(R.id.txtPeso);
         txtAltura = findViewById(R.id.txtAltura);
-        btnCalcularIMC= findViewById(R.id.btnCalcularIMC);
+        txtEdad = findViewById(R.id.txtEdad);
+        btnCalcularIMC = findViewById(R.id.btnCalcularIMC);
         textViewResultado = findViewById(R.id.textViewResultado);
+        textViewSituacion = findViewById(R.id.textViewSituacion);
+        altura = findViewById(R.id.spinnerAltura);
+        peso = findViewById(R.id.spinnerPeso);
 
         // Agregar listeners a los botones de género
         btnMasculino.setOnClickListener(new View.OnClickListener() {
@@ -56,41 +65,171 @@ public class MainActivity extends AppCompatActivity {
                 calcularIMC();
             }
         });
+
+        // Configurar el ArrayAdapter para los Spinners de altura y peso
+        ArrayAdapter<CharSequence> alturaAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.altura_options,
+                android.R.layout.simple_spinner_item
+        );
+        alturaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        altura.setAdapter(alturaAdapter);
+
+        ArrayAdapter<CharSequence> pesoAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.peso_options,
+                android.R.layout.simple_spinner_item
+        );
+        pesoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        peso.setAdapter(pesoAdapter);
+
+        // Agregar listener al Spinner de altura
+        altura.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedAltura = parent.getItemAtPosition(position).toString();
+                // Realizar la conversión de la altura según la opción seleccionada (cm o pulgadas)
+                // y actualizar el EditText de altura
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // No se realiza ninguna acción
+            }
+        });
+
+        // Agregar listener al Spinner de peso
+        peso.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedPeso = parent.getItemAtPosition(position).toString();
+                // Realizar la conversión del peso según la opción seleccionada (kg o libras)
+                // y actualizar el EditText de peso
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // No se realiza ninguna acción
+            }
+        });
     }
 
     private void calcularIMC() {
+        double peso = Double.parseDouble(txtPeso.getText().toString());
+        double altura = Double.parseDouble(txtAltura.getText().toString());
+        edad = Integer.parseInt(txtEdad.getText().toString());
 
-       if(selectedGender=="femenino"){
-           double peso = Double.parseDouble(txtPeso.getText().toString());
-           double altura = Double.parseDouble(txtAltura.getText().toString());
+        // Cálculo del IMC
+        double imc = peso / (altura * altura);
 
+        // Determinar la situación según el IMC, el género y la edad
+        String situacion = "";
+        if (selectedGender.equals("femenino")) {
+            if (edad < 16) {
+                if (imc < 19) {
+                    situacion = "Delgadez severa";
+                } else if (imc >= 19 && imc < 24) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            } else if (edad >= 16 && edad <= 18) {
+                if (imc < 19) {
+                    situacion = "Delgadez severa";
+                } else if (imc >= 19 && imc < 24) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            } else if (edad >= 19 && edad <= 24) {
+                if (imc < 19) {
+                    situacion = "Delgadez";
+                } else if (imc >= 19 && imc < 24) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            } else if (edad >= 25 && edad <= 29) {
+                if (imc < 20) {
+                    situacion = "Delgadez";
+                } else if (imc >= 20 && imc < 25) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            } else if (edad >= 30 && edad <= 60) {
+                if (imc < 21) {
+                    situacion = "Delgadez";
+                } else if (imc >= 21 && imc < 26) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            } else if (edad > 60) {
+                if (imc < 22) {
+                    situacion = "Delgadez";
+                } else if (imc >= 22 && imc < 27) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            }
+        } else if (selectedGender.equals("masculino")) {
+            if (edad < 16) {
+                if (imc < 20) {
+                    situacion = "Delgadez severa";
+                } else if (imc >= 20 && imc < 25) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            } else if (edad >= 16 && edad <= 18) {
+                if (imc < 20) {
+                    situacion = "Delgadez severa";
+                } else if (imc >= 20 && imc < 25) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            } else if (edad >= 19 && edad <= 24) {
+                if (imc < 20) {
+                    situacion = "Delgadez";
+                } else if (imc >= 20 && imc < 25) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            } else if (edad >= 25 && edad <= 29) {
+                if (imc < 21) {
+                    situacion = "Delgadez";
+                } else if (imc >= 21 && imc < 26) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            } else if (edad >= 30 && edad <= 60) {
+                if (imc < 22) {
+                    situacion = "Delgadez";
+                } else if (imc >= 22 && imc < 27) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            } else if (edad > 60) {
+                if (imc < 23) {
+                    situacion = "Delgadez";
+                } else if (imc >= 23 && imc < 28) {
+                    situacion = "Peso normal";
+                } else {
+                    situacion = "Sobrepeso";
+                }
+            }
+        }
 
-           double imc = peso / (altura * altura);
-
-
-           String resultado = String.format("Tu IMC es", imc, selectedGender);
-           Toast.makeText(this, resultado, Toast.LENGTH_SHORT).show();
-           textViewResultado.setText(resultado);
-
-
-       }
-       if (selectedGender=="masculino"){
-
-
-
-           double peso = Double.parseDouble(txtPeso.getText().toString());
-           double altura = Double.parseDouble(txtAltura.getText().toString());
-
-
-           double imc = peso / (altura * altura);
-
-
-           String resultado = String.format("Tu IMC es", imc, selectedGender);
-           Toast.makeText(this, resultado, Toast.LENGTH_SHORT).show();
-           textViewResultado.setText(resultado);
-       }
-
-
-
+        // Mostrar el IMC y la situación en los TextView correspondientes
+        textViewResultado.setText(String.format("%.2f", imc));
+        textViewResultado.setVisibility(View.VISIBLE);
+        textViewSituacion.setText(situacion);
+        textViewSituacion.setVisibility(View.VISIBLE);
     }
 }
